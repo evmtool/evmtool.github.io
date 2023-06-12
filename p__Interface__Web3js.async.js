09bd12ff8ab27b1385bbc32333d9b8f69c229378
evmtool <133187965+evmtool@users.noreply.github.com>
2023-06-12 15:57:57 +0800

    Add files via upload

---
+++ p__Interface__Web3js.async.js
@@ -0,0 +1,15 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[460],{62426:function(b,i,n){n.r(i);var m=n(51877),r=n(78565),a=n(58676),l=n(26713),d=n(37918),s=n.n(d),t=n(86074),e=new(s()),c=function(){return(0,t.jsx)(r._z,{ghost:!0,className:"interface",children:(0,t.jsxs)(l.Z,{className:"mb-[8px]",direction:"vertical",size:"middle",style:{display:"flex"},children:[(0,t.jsx)(a.K1,{title:"web3.eth.abi.encodeFunctionSignature(funcName)",func:e.eth.abi.encodeFunctionSignature.bind(e.eth.abi),placeholder:'example: "myFunction(uint256,uint32[],bytes10,bytes)"'}),(0,t.jsx)(a.K1,{title:"web3.eth.abi.encodeEventSignature(eventName)",func:e.eth.abi.encodeEventSignature.bind(e.eth.abi),placeholder:'example: "myEvent(uint256,bytes32)"'}),(0,t.jsx)(a.K1,{title:"web3.eth.abi.encodeParameter(type, parameter)",func:e.eth.abi.encodeParameter.bind(e.eth.abi),placeholder:'example: "uint256", 123'}),(0,t.jsx)(a.K1,{title:"web3.eth.abi.encodeParameters(typesArray, parameters)",func:e.eth.abi.encodeParameters.bind(e.eth.abi),placeholder:'example: ["uint256","string"], [2345675643, "Hello!%"]'}),(0,t.jsx)(a.K1,{title:"web3.eth.abi.encodeFunctionCall(jsonInterface, parameters)",func:e.eth.abi.encodeFunctionCall.bind(e.eth.abi),placeholder:'example: {"name": "myMethod", "type": "function", "inputs": [{"type": "uint256", "name": "myNumber"},{"type": "string","name": "myString"}]}, [2345675643, "Hello!%"]'}),(0,t.jsx)(a.K1,{title:"web3.eth.abi.decodeParameter(type, hexString)",func:e.eth.abi.decodeParameter.bind(e.eth.abi),placeholder:'example: "uint256", "0x0000000000000000000000000000000000000000000000000000000000000010"'}),(0,t.jsx)(a.K1,{title:"web3.eth.abi.decodeParameters(typesArray, hexString)",func:e.eth.abi.decodeParameters.bind(e.eth.abi),stringify:!0,placeholder:'example: ["bytes1", "uint8"], "0x1200000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000007b"'}),(0,t.jsx)(a.K1,{title:"web3.eth.abi.decodeLog(inputs, hexString, topics)",func:e.eth.abi.decodeLog.bind(e.eth.abi),stringify:!0,placeholder:`example: 
+[{
+  "type": "string",
+  "name": "myString"
+},{
+  "type": "uint256",
+  "name": "myNumber",
+  "indexed": true
+},{
+  "type": "uint8",
+  "name": "mySmallNumber",
+  "indexed": true
+}],
+"0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000748656c6c6f252100000000000000000000000000000000000000000000000000",
+["0x000000000000000000000000000000000000000000000000000000000000f310", "0x0000000000000000000000000000000000000000000000000000000000000010"]`})]})})};i.default=c}}]);
